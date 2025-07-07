1094ea9269c32668475db0860c7d82f53a8ebeba
SopnilSwapon <sopnilswapon1@gmail.com>
2025-07-07 09:12:53 +0600

    created automatic file database system

--- index.js
+++ index.js
@@ -2,25 +2,34 @@
 
 // dependencies
 
-const http = require("http");
+const http = require('http');
 
-const {handleReqRes} = require('./helpers/handleReqRes');
-const environments = require("./helpers/environments")
+const x = 5;
+
+const { handleReqRes } = require('./helpers/handleReqRes');
+const environments = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
 
-// create server 
+data.create(
+  'test',
+  'newFile',
+  { name: 'Bangladesh', language: 'Bengali' },
+  (err) => {
+    console.log(`The error was ${err}`);
+  }
+);
+// create server
 app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environments.port, () => {
-        console.log(`environment is ${process.env.NODE_ENV}`)
-        console.log(`listing to  port ${environments.port}`)
-    }) ;
-}
+  const server = http.createServer(app.handleReqRes);
+  server.listen(environments.port, () => {
+    console.log(`environment is ${process.env.NODE_ENV}`);
+    console.log(`listing to  port ${environments.port}`);
+  });
+};
 
-app.handleReqRes = handleReqRes
+app.handleReqRes = handleReqRes;
 
 app.createServer();
-
-
